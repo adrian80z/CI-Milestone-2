48509733030ac38895d7e92f0d214efe0f21fd34
Adrian <adrian.ziolkowski@gmail.com>
2019-12-13 20:12:49 +0000

    hide sentence code add

--- assets/js/scirpts.js
+++ assets/js/scirpts.js
@@ -1,10 +1,27 @@
 var sentence = "Better late than never";
 
+//change sentence to uppercase letters
+sentence = sentence.toUpperCase();
+
+//checking the length of sentence
+var sentenceLenth = sentence.length;
+
+//creating variable to keep hidden sequence
+var hiddenSentence = "";
+
+//looping through string and replacing letters and spaces adequately
+for(i = 0; i < sentenceLenth; i++){
+    if(sentence.charAt(i) == " ") {
+        hiddenSentence = hiddenSentence + " ";
+    } else {
+        hiddenSentence = hiddenSentence + "-";
+    }
+}
 
 //attach sentence to div 
-function write_sentence(){
-    document.getElementById("sentence-board").innerHTML = sentence;
+function writeSentence(){
+    document.getElementById("sentence-board").innerHTML = hiddenSentence;
 }
 
 //shows sentence after page load
-window.onload = write_sentence;
+window.onload = writeSentence;
