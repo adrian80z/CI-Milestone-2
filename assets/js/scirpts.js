4313d26908bbf6cf4b5547eabe989eac94887d39
Adrian <adrian.ziolkowski@gmail.com>
2019-12-19 00:04:27 +0000

    drawning hangman code added

--- assets/js/scirpts.js
+++ assets/js/scirpts.js
@@ -1,4 +1,4 @@
-var sentence = "Better";
+var sentence = "No gain without pain";
 
 //change sentence to uppercase letters
 sentence = sentence.toUpperCase();
@@ -91,11 +91,7 @@ function checkLetter(num){
         document.getElementById("alphabet").innerHTML = 'You Won!!! <br/><br/><span class="reset" onclick="window.location.reload()">Play Again?</span>';
     }
 
-    //game over
-    if(incorrectTurns == 8) {
-        document.getElementById("message").innerHTML = "You've last chance to guess!!!";
-    }
-
+    //fail
     var drawOnCanvas = document.getElementById("drawGibbet");
     var ctx = drawOnCanvas.getContext('2d');
 
@@ -103,43 +99,134 @@ function checkLetter(num){
     if(incorrectTurns==1){
         ctx.lineWidth = 20;
         ctx.beginPath(); //vertical bar
-        ctx.moveTo(50,270);
-        ctx.lineTo(50,25);
-        ctx.stroke();
+            ctx.moveTo(50,270);
+            ctx.lineTo(50,25);
+            ctx.stroke();
     }
 
     if(incorrectTurns==2){
         ctx.lineWidth = 20;
         ctx.beginPath(); //horizontal bar
-        ctx.moveTo(29,25);
-        ctx.lineTo(175,25);
-        ctx.stroke();  
+            ctx.moveTo(29,25);
+            ctx.lineTo(175,25);
+            ctx.stroke();  
     }
 
     if(incorrectTurns==3){
         ctx.lineWidth = 10;
         ctx.beginPath(); //cross bar
-        ctx.moveTo(50,80);
-        ctx.lineTo(100,25);
-        ctx.stroke(); 
+            ctx.moveTo(50,80);
+            ctx.lineTo(100,25);
+            ctx.stroke(); 
     }
 
     if(incorrectTurns==4){
         ctx.lineWidth = 5;
         ctx.beginPath(); //line
-        ctx.moveTo(150,30);
-        ctx.lineTo(150,80);
-        ctx.stroke();
+            ctx.moveTo(150,30);
+            ctx.lineTo(150,80);
+            ctx.stroke();
     }
 
+    if(incorrectTurns==5){
+        ctx.lineWidth = 3;
+        ctx.beginPath(); //head
+            ctx.arc(150, 100, 20, 0, 2*Math.PI);
+            ctx.stroke();
+        ctx.beginPath(); //left eye
+            ctx.arc(143, 95, 3.5, 0, 2*Math.PI);
+            ctx.stroke();
+        ctx.beginPath(); //right eye
+            ctx.arc(157, 95, 3.5, 0, 2*Math.PI);
+            ctx.stroke();
+        ctx.beginPath(); //mouth
+            ctx.arc(150, 103, 9, 0, Math.PI);
+            ctx.stroke();
+         ctx.beginPath(); //body
+            ctx.moveTo(150,120);
+            ctx.lineTo(150,190);
+            ctx.stroke();
+    }
 
+    if(incorrectTurns==6){
+        ctx.lineWidth = 3;
+        ctx.beginPath(); //right arm
+            ctx.moveTo(150,135);
+            ctx.lineTo(180,160);
+            ctx.stroke();   
+    }
 
+    if(incorrectTurns==7){
+        ctx.lineWidth=3;
+        ctx.fillStyle = "#CECCCC";
+        ctx.fillRect(138, 102, 24, 12); //cover mouth
+        ctx.beginPath(); //straight mouth
+            ctx.moveTo(140,108);
+            ctx.lineTo(160,108);
+            ctx.stroke();
+        ctx.beginPath(); //left arm
+            ctx.moveTo(150,135);
+            ctx.lineTo(120,160);
+            ctx.stroke();
+    }
 
+    if(incorrectTurns==8){
+        document.getElementById("message").innerHTML = "You've last chance to guess!!!";
+        
+        ctx.lineWidth = 3;
+        ctx.beginPath(); //right leg
+            ctx.moveTo(149,188);
+            ctx.lineTo(180,230);
+            ctx.stroke();
+        ctx.beginPath(); //left eye
+            ctx.moveTo(140,93);
+            ctx.lineTo(146,98);
+            ctx.stroke();
+            ctx.moveTo(140,98);
+            ctx.lineTo(146,93);
+            ctx.stroke();
+        ctx.beginPath(); //right eye
+            ctx.moveTo(154,98);
+            ctx.lineTo(160,93);
+            ctx.stroke(); 
+            ctx.moveTo(154,93);
+            ctx.lineTo(160,98);
+            ctx.stroke();
+        ctx.fillRect(138, 102, 24, 12); //cover mouth
+        ctx.beginPath(); //sad mouth
+            ctx.arc(150, 112, 9, 0, Math.PI, true);
+            ctx.stroke();
+    }
 
-    else if(incorrectTurns >= 9) {
+    if(incorrectTurns==9){
         document.getElementById("message").style.display="none";
         document.getElementById("alphabet").innerHTML = 'Game Over!!! <br/> Correct sentence is: '+ sentence +' <br/><br/><span class="reset" onclick="window.location.reload()">Play Again?</span>';
-    }  
+
+        ctx.lineWidth = 3;
+        ctx.beginPath(); //left leg
+            ctx.moveTo(151,188);
+            ctx.lineTo(120,230);
+            ctx.stroke();
+        ctx.fillRect(138, 90, 24, 24); //cover face
+        ctx.beginPath(); //straight mouth
+            ctx.moveTo(140,108);
+            ctx.lineTo(160,108);
+            ctx.stroke();
+        ctx.beginPath(); //left eye
+            ctx.moveTo(140,93);
+            ctx.lineTo(146,98);
+            ctx.stroke();
+            ctx.moveTo(140,98);
+            ctx.lineTo(146,93);
+            ctx.stroke();
+        ctx.beginPath(); //right eye
+            ctx.moveTo(154,98);
+            ctx.lineTo(160,93);
+            ctx.stroke(); 
+            ctx.moveTo(154,93);
+            ctx.lineTo(160,98);
+            ctx.stroke();
+    }
 }
 
     function drawBase(){
